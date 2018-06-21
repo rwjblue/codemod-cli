cf93aeb13d531d3dcb3bae349ebe995c81ee87ea
Robert Jackson <rjackson@linkedin.com>
2018-06-21 14:20:52 -0400

    Use liftoff to defer to locally installed version.

--- cli.js
+++ cli.js
@@ -6,9 +6,18 @@
 // eslint-disable-next-line no-global-assign
 require = require('esm')(module /*, options*/);
 
-require('yargs')
+const args = require('yargs')
   .commandDir('commands')
   .demandCommand()
   .help()
-  .epilog('For more information, see https://github.com/rwjblue/codemod-cli')
-  .parse();
+  .epilog('For more information, see https://github.com/rwjblue/codemod-cli');
+
+const Liftoff = require('liftoff');
+let CodemodCLI = new Liftoff({
+  name: 'codemod-cli',
+  configName: '.codemod-cli',
+});
+
+CodemodCLI.launch({}, (/* env */) => {
+  args.parse();
+});
