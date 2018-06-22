a7b773609302ebe1a8c4ebd2716bcdcde114f0a8
Robert Jackson <rjackson@linkedin.com>
2018-06-22 11:09:08 -0400

    Ensure requires are lazy to use local version.

--- bin/cli.js
+++ bin/cli.js
@@ -3,15 +3,6 @@
 
 /* eslint-env node */
 
-// eslint-disable-next-line no-global-assign
-require = require('esm')(module /*, options*/);
-
-const args = require('yargs')
-  .commandDir('../commands')
-  .demandCommand()
-  .help()
-  .epilog('For more information, see https://github.com/rwjblue/codemod-cli');
-
 const Liftoff = require('liftoff');
 let CodemodCLI = new Liftoff({
   name: 'codemod-cli',
@@ -19,5 +10,14 @@ let CodemodCLI = new Liftoff({
 });
 
 CodemodCLI.launch({}, (/* env */) => {
+  // eslint-disable-next-line no-global-assign
+  require = require('esm')(module /*, options*/);
+
+  const args = require('yargs')
+    .commandDir('../commands')
+    .demandCommand()
+    .help()
+    .epilog('For more information, see https://github.com/rwjblue/codemod-cli');
+
   args.parse();
 });
