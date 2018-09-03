4178924d718293a5b596ef1c6fe7b58e901aa782
Santosh Sutar <ssutar@ssutar-mn4.linkedin.biz>
2018-09-02 23:24:49 -0700

    Update transform to include subfolders from the testfixtures in the test run
    Use globby to include tests from subfolders in the __testfixtures__

--- tests/cli-test.js
+++ tests/cli-test.js
@@ -225,6 +225,48 @@ QUnit.module('codemod-cli', function(hooks) {
           assert.equal(result.code, 0, 'exited with zero');
         })
       );
+
+      QUnit.test(
+        'transform should receive a subfolder file path in tests',
+        wrap(function*(assert) {
+          const realCodemodProjectPath = fs.realpathSync(codemodProject.path());
+          const expectedPath = `${realCodemodProjectPath}/transforms/main/__testfixtures__/foo/basic.input.js`;
+
+          yield execa(EXECUTABLE_PATH, ['generate', 'codemod', 'main']);
+
+          codemodProject.write({
+            transforms: {
+              main: {
+                'index.js': `
+                  const { getParser } = require('codemod-cli').jscodeshift;
+
+                  module.exports = function transformer(file, api) {
+                    const j = getParser(api);
+
+                    return j(file.source)
+                    .find(j.Literal)
+                    .forEach(path => {
+                      path.replace(
+                        j.stringLiteral(file.path)
+                      );
+                    })
+                    .toSource();
+                  }
+                `,
+                __testfixtures__: {
+                  foo: {
+                    'basic.input.js': 'var foo = "foo";',
+                    'basic.output.js': `var foo = "${expectedPath}";`,
+                  },
+                },
+              },
+            },
+          });
+
+          let result = yield execa(EXECUTABLE_PATH, ['test']);
+          assert.equal(result.code, 0, 'exited with zero');
+        })
+      );
     });
   });
 
